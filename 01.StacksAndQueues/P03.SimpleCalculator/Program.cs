612bd081ff9fd0dbc3d09efe5f51e99b70d3ed58
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-17 23:20:44 +0300

    Commit.

---
+++ 01.StacksAndQueues/P03.SimpleCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expr = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < expr.Length; i++)
+            {
+                
+            }
+
+        }
+    }
+}
---
+++ 01.StacksAndQueues/P04.MatchingBrackets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace P04.MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expr = Console.ReadLine();
+            var stack = new Stack<int>();
+            for (int i = 0; i < expr.Length; i++)
+            {
+                char ch = expr[i];
+                if (ch == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (ch == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+                    string subexpr = expr.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(subexpr);
+                }
+            }
+
+        }
+    }
+}
