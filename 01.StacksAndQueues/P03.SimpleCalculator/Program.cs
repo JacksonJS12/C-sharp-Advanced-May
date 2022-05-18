cd970c447a53ebce2884ccc17c661a271aa8cb42
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-18 16:08:13 +0300

    Commit.

--- 01.StacksAndQueues/P03.SimpleCalculator/Program.cs
+++ 01.StacksAndQueues/P03.SimpleCalculator/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace P03.SimpleCalculator
 {
@@ -9,37 +6,7 @@ namespace P03.SimpleCalculator
     {
         static void Main(string[] args)
         {
-            string[] expr = Console.ReadLine()
-                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            var reversed = expr.Reverse().ToArray();
-            Stack<string> stack = new Stack<string>();
-            for (int i = 0; i < reversed.Length; i++)
-            {
-                stack.Push(reversed[i]);
-            }
-            for (int i = 0; i < reversed.Length; i++)
-            {
-                int sum = 0;
-                string plusOrMinus = string.Empty;
-                if (expr[i] == "+" || expr[i] == "-")
-                {
-                    plusOrMinus = stack.Pop();
-                }
-                else
-                {
-                    int lastNum = 0;
-                    if (!(plusOrMinus == string.Empty))
-                    {
-                        sum = lastNum + int.Parse(stack.Pop());
-                         lastNum = int.Parse(plusOrMinus + stack.Pop());
-                    }
-                    else
-                    {
-                         lastNum = int.Parse(stack.Pop());
-                    }
-                  
-                }
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
