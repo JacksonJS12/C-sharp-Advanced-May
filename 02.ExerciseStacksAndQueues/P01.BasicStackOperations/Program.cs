d250de1113c68925487a4434636357ce6aad3090
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-18 21:55:32 +0300

    Commit.

--- 02.ExerciseStacksAndQueues/P01.BasicStackOperations/Program.cs
+++ 02.ExerciseStacksAndQueues/P01.BasicStackOperations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P01.BasicStackOperations
 {
@@ -6,7 +8,46 @@ namespace P01.BasicStackOperations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> stack = new Stack<int>();
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int nToPush = numbers[0];
+            int sToPop = numbers[1];
+            int xToFind = numbers[2];
+
+            List<int> numberList = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 0; i < nToPush; i++)
+            {
+                stack.Push(numberList[i]);
+            }
+
+            for (int i = 0; i < sToPop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                if (stack.Contains(xToFind))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
         }
     }
 }
