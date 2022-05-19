a510addead73a4e8a4a59d4d27bba08139a7bcdf
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-19 23:56:37 +0300

    Exercise 06 complete.

---
+++ 02.ExerciseStacksAndQueues/P06.SongsQueue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06.SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songsQueue = new Queue<string>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries));
+
+            
+            while (songsQueue.Count > 0)
+            {
+                string cmd = Console.ReadLine();
+
+                if (cmd == "Play")
+                {
+                    songsQueue.Dequeue();
+                }
+                else if (cmd.Contains("Add"))
+                {
+                    string song = cmd.Substring(4);
+                    if (songsQueue.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        songsQueue.Enqueue(song);
+                    }
+                }
+                else if(cmd == "Show")
+                {
+                    Console.Write(string.Join(", ", songsQueue));
+                }
+               
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
