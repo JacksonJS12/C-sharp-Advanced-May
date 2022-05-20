c85b9296449bd224f4fc1d7657ab0245b3b822fe
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-20 23:34:34 +0300

    Commit.

---
+++ 03.MultidimensionalArrays/P01. SumMatrixElements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01._SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
