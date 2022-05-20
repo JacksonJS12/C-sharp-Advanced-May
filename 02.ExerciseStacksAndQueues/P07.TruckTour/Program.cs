81b65c718df0e3d8edbc259230a525c7192f1c03
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-20 17:11:23 +0300

    Exercise 09 complete.

---
+++ 02.ExerciseStacksAndQueues/P07.TruckTour/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.TruckTour
+{
+    internal class Program
+    {
+        class PetrolPump
+        {
+            public PetrolPump(int number, int amountOfPetrolForPump, int dictanceBetweenFromPetrolPumpToAnother)
+            {
+                this.Number = number;
+                this.AmountOfPetrolForPump = amountOfPetrolForPump;
+                this.DictanceBetweenFromPetrolPumpToNext = dictanceBetweenFromPetrolPumpToAnother;
+            }
+            public int Number { get; set; }
+            public int AmountOfPetrolForPump { get; set; }
+            public int DictanceBetweenFromPetrolPumpToNext { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<PetrolPump> petrolPumpsQueue = new Queue<PetrolPump>();
+
+            for (int i = 0; i <= n-1; i++)
+            {
+                int[] petrolPumpsInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int amountOfPetrolForPump = petrolPumpsInfo[0];
+                int dictanceBetweenFromPetrolPumpToAnother = petrolPumpsInfo[1];
+
+                PetrolPump petrolPump = new PetrolPump(i, amountOfPetrolForPump, dictanceBetweenFromPetrolPumpToAnother);
+                petrolPumpsQueue.Enqueue(petrolPump);
+            }
+            int totalDistance = petrolPumpsQueue.Sum(petrolPump => petrolPump.DictanceBetweenFromPetrolPumpToNext);
+            int truckDistance = 0;
+
+            while (true)
+            {
+            int truckFuel = 0;
+                foreach (var petrolPump in petrolPumpsQueue)
+                {
+                    truckFuel += petrolPump.AmountOfPetrolForPump;
+                    if (truckFuel - petrolPump.DictanceBetweenFromPetrolPumpToNext > 0)
+                    {
+
+                    }
+                    truckDistance += petrolPump.DictanceBetweenFromPetrolPumpToNext;
+                }
+               
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 02.ExerciseStacksAndQueues/P09.SimpleTextEditor/Program.cs
@@ -0,0 +1,44 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P09.SimpleTextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           StringBuilder text = new StringBuilder();
+
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string cmd = Console.ReadLine();
+                // string cmdType = cmd.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0];
+                if (cmd.StartsWith("1"))
+                {
+                    stack.Push(text.ToString());
+                    string textForAdd = cmd.Split()[1];
+                    text.Append(textForAdd);
+                }
+                else if (cmd.StartsWith("2"))
+                {
+                    stack.Push(text.ToString());
+                    int count = int.Parse(cmd.Split()[1]);
+                    text.Remove(text.Length - count, count);
+                }
+                else if (cmd.StartsWith("3"))
+                {
+                    int index = int.Parse(cmd.Split()[1]);
+                    Console.WriteLine(text[index-1]);
+                }
+                else if (cmd.StartsWith("4"))
+                {
+                    text = new StringBuilder(stack.Pop());
+                }
+               
+            }
+        }
+    }
+}
