5d7f17c41c184238cd11990832fafda8c099cd94
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-21 12:59:50 +0300

    Commit.

---
+++ 03.MultidimensionalArrays/P02.SumMatrixColumns/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Linq;
+
+namespace P02.SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+               .Split(", ")
+               .Select(int.Parse)
+               .ToArray();
+
+            (int rowsCount, int colsCount) = (dimensions[0], dimensions[1]);
+
+            int[,] matrix = new int[rowsCount, colsCount];
+            for (int row = 0; row < rowsCount; row++)
+            {
+                int[] line = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+                for (int col = 0; col < colsCount; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            long[] colSums = new long[colsCount];
+
+            for (int row = 0; row < rowsCount; row++)
+            {
+                for (int col = 0; col < colsCount; col++)
+                {
+                    colSums[col] += matrix[row, col];
+                }
+            }
+
+            for (int col = 0; col < colsCount; col++)
+            {
+                Console.WriteLine(colSums[col]);
+            }
+        }
+    }
+}
