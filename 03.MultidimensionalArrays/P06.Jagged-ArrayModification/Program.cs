3c2339102025a3d4b9060be3276876164be6cb43
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-22 17:40:23 +0300

    Exercise 06 complete.

---
+++ 03.MultidimensionalArrays/P06.Jagged-ArrayModification/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace P06.Jagged_ArrayModification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCount = int.Parse(Console.ReadLine());
+            int[][] jaggedArray = new int[rowsCount][];
+            for (int i = 0; i < rowsCount; i++)
+            {
+                jaggedArray[i] = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+            while (true)
+            {
+                string[] cmd = Console.ReadLine()
+                    .Split(' ');
+                if (cmd[0] == "END")
+                {
+                    break;
+                }
+                if (cmd[0] == "Add" || cmd[0] == "Subtract")
+                {
+                    int row = int.Parse(cmd[1]);
+                    int col = int.Parse(cmd[2]);
+                    int value = int.Parse(cmd[3]);
+                    if (cmd[0] == "Subtract")
+                    {
+                       value = -value;
+                    }
+                    if (row >= 0 && 
+                        row < jaggedArray.Length && 
+                        col >= 0 &&
+                        col < jaggedArray[row].Length)
+                    {
+                        jaggedArray[row][col] += value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                }
+              
+            }
+            foreach (var row in jaggedArray)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
