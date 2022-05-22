e0d5e58c65263b80a40de7b470ac6e83be8d3264
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-22 17:54:56 +0300

    Exercise 07 complete.

---
+++ 03.MultidimensionalArrays/P07.PascalTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+namespace P07.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger[][] tringle = new BigInteger[n][];
+            for (int row = 0; row < n; row++)
+            {
+                tringle[row] = new BigInteger[row + 1]; 
+                tringle[row][0] = 1;
+                for (int col = 1; col < row; col++)
+                {
+                    tringle[row][col] =
+                        tringle[row - 1][col - 1] + tringle[row - 1][col];
+                }
+                tringle[row][row] = 1;
+            }
+
+            foreach (var row in tringle)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
