cb835faf5a4912c3b6b10f6d6c772b9acb3fd0db
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-23 23:54:45 +0300

    Commit.

---
+++ 04.ExerciseMultidimensionalArrays/P04.MatrixShuffling/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace P04.MatrixShuffling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dimensions = Console.ReadLine();
+            int rowsCount = int.Parse(dimensions.Split()[0]);
+            int columnsCount = int.Parse(dimensions.Split()[1]);
+
+            string[,] matrix = new string[rowsCount, columnsCount];
+            //Fill the matrix
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] elements = Console.ReadLine().Split();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+
+            string cmd = Console.ReadLine();
+            while (cmd != "END")
+            {
+                
+
+                cmd = Console.ReadLine();
+            }
+
+        }
+
+        static bool Validate(string cmd)
+        {
+            string[] cmdParts = cmd
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+            if (cmd.Contains("swap") && cmdParts.Length == 5)
+            {
+                int row1 = int.Parse(cmdParts[1]);
+                int col1 = int.Parse(cmdParts[1]);
+                int row2 = int.Parse(cmdParts[1]);
+                int col2 = int.Parse(cmdParts[1]);
+            }
+        }
+    }
+}
