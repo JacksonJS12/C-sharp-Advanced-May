488d11ebeb7211e64cceb91a18e8e0fa459cda51
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-25 01:44:35 +0300

    Exercise 06 completed.

---
+++ 05.SetsAndDictionariesAdvanced/P06.RecordUniqueNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06.RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new HashSet<string>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
