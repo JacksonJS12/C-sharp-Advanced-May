2943dca34eb843f1873306391d50a77909dca591
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-24 22:30:56 +0300

    Exercise 03 completed.

---
+++ 05.SetsAndDictionariesAdvanced/P03.Largest-3-Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+              .Split(' ')
+              .Select(int.Parse)
+                .OrderByDescending(x => x)
+                .Take(3)
+               .ToList();
+
+            Console.WriteLine(String.Join(" ", input));
+
+        }
+    }
+}
