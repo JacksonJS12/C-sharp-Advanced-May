fa38daf45ee0d6107a63be302e7a5b1a8376f64e
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-25 00:08:20 +0300

    Exercise 02 completed.

---
+++ 05.SetsAndDictionariesAdvanced/P02.AverageStudentGrades/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.AverageStudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentGrades = new Dictionary<string, List<decimal>>();
+            int gradesCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < gradesCount; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                string name = line[0];
+                decimal grade = decimal.Parse(line[1]);
+                if (!studentGrades.ContainsKey(name))
+                    studentGrades.Add(name, new List<decimal>());
+
+                studentGrades[name].Add(grade);
+            }
+
+            foreach (var name in studentGrades.Keys)
+            {
+                List<decimal> gardes = studentGrades[name];
+                string grades = string.Join(" ", gardes.Select(g => g.ToString("f2")));
+                decimal avg = Math.Round(studentGrades[name].Average(), 2, MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{name} -> {grades} (avg: {avg:f2})");
+            }
+        }
+    }
+}
