1d86d8e23a389bf2ba2b7fefc52e2d1304f96a07
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-26 23:44:15 +0300

    Commit.

--- 04.ExerciseMultidimensionalArrays/P03.MaximalSum/Program.cs
+++ 04.ExerciseMultidimensionalArrays/P03.MaximalSum/Program.cs
@@ -25,23 +25,38 @@ namespace P03.MaximalSum
                 }
             }
 
-            int[,] square3x3Matrix = new int[3,3];
+            int[,] square3x3Matrix = new int[3, 3];
+            int maxSum = 0;
             int sum = 0;
-            for (int row = 1; row < rowsCount; row++)
+
+            for (int row = 0; row < rowsCount - 2; row++)
             {
-                for (int col = 1; col < colsCount-1; col++)
+
+                for (int col = 0; col < colsCount - 2; col++)
                 {
-                    square3x3Matrix[row - 1, col - 1] = matrix[row, col];
-                    sum += matrix[row, col];
+                    square3x3Matrix[row, col] = matrix[row, col];
+                    square3x3Matrix[row + 1, col] = matrix[row + 1, col+1];
+                    square3x3Matrix[row + 2, col] = matrix[row + 2, col+2];
+                    sum += matrix[row, col] + matrix[row + 1, col+1] + matrix[row + 2, col+2];
+
+
                 }
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    square3x3Matrix = new int[3, 3];
+
+                }
+                sum = 0;
+
             }
 
-            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Sum = {maxSum}");
             for (int row = 0; row < square3x3Matrix.GetLength(0); row++)
             {
                 for (int col = 0; col < square3x3Matrix.GetLength(1); col++)
                 {
-                    Console.Write(square3x3Matrix[row,col] + " ");
+                    Console.Write(square3x3Matrix[row, col] + " ");
                 }
                 Console.WriteLine();
             }
