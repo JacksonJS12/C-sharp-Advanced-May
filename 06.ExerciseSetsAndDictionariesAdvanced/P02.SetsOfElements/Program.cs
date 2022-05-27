0a309c0526ae86b5c722f54e54042dc26a051dda
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-27 23:17:08 +0300

    Exercise 02 completed.

---
+++ 06.ExerciseSetsAndDictionariesAdvanced/P02.SetsOfElements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = (Console.ReadLine());
+            int firstSetLength = int.Parse(input.Split(' ')[0]);
+            int secondSetLength = int.Parse(input.Split(' ')[1]);
+
+            HashSet<int> firstSet = new HashSet<int>();
+            for (int i = 0; i < firstSetLength; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            HashSet<int> secondSet = new HashSet<int>();
+            for (int i = 0; i < secondSetLength; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            firstSet.IntersectWith(secondSet);
+
+            Console.WriteLine(string.Join(" ", firstSet));
+            
+        }
+    }
+}
