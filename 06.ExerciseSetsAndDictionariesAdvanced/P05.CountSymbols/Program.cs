a56c7c7f7d39feaa18aa3dcd0f4bc7e382ed9b83
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-28 17:24:47 +0300

    Exercise 05 completed.

---
+++ 06.ExerciseSetsAndDictionariesAdvanced/P05.CountSymbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> times = new SortedDictionary<char, int>();
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!times.ContainsKey(input[i]))
+                {
+                    times.Add(input[i], 1);
+                }
+                else
+                {
+                    times[input[i]]++;
+                }
+                
+            }
+
+            foreach (var time in times.Keys)
+            {
+                Console.WriteLine($"{time}: {times[time]} time/s");
+            }
+        }
+    }
+}
