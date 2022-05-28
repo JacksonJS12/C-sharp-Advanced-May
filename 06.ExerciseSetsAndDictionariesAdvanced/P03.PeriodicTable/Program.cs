f2fb7f9d62a480f8a015bdefb2c0c729035d23e2
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-28 16:37:40 +0300

    Exercise 03 completed.

---
+++ 06.ExerciseSetsAndDictionariesAdvanced/P03.PeriodicTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> table = new SortedSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                foreach (var element in elements)
+                {
+                    table.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", table));
+        }
+    }
+}
