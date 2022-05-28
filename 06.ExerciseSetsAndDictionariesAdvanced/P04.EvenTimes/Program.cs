d7a8b25787fc0d0eac96e094578125d19643315d
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-28 17:06:52 +0300

    Exercise 04 completed.

---
+++ 06.ExerciseSetsAndDictionariesAdvanced/P04.EvenTimes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> evenNumsAndTimes = new Dictionary<int, int>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (!evenNumsAndTimes.ContainsKey(number))
+                {
+                    evenNumsAndTimes.Add(number, 1);
+                   
+                }
+                else
+                {
+                    evenNumsAndTimes[number]++;
+                }
+            }
+            
+            Console.WriteLine(evenNumsAndTimes
+                .First(num => num.Value % 2 == 0)
+                .Key); 
+        }
+    }
+}
