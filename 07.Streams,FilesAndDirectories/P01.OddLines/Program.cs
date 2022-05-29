534764124d95fbcfef74299d2c8e9b0fffaf5dbe
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-29 21:50:46 +0300

    Ex01 completed.

--- 07.Streams,FilesAndDirectories/P01.OddLines/Program.cs
+++ 07.Streams,FilesAndDirectories/P01.OddLines/Program.cs
@@ -1,13 +1,44 @@
-﻿using System;
-using System.IO;
-
-namespace P01.OddLines
+﻿
+namespace OddLines
 {
-    internal class Program
+    using System.IO;
+
+    public class OddLines
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader("../../../input.txt");
+            using (reader)
+            {
+                var writer = new StreamWriter("ouput.txt");
+                using (writer)
+                {
+                    while (true)
+                    {
+                        int lineNum = 0;
+                        string line = reader.ReadLine();
+                        while (line != null)
+                        {
+                            if (lineNum % 2 == 1)
+                            {
+                                writer.WriteLine(line);
+                                writer.Flush();
+                            }
+                            lineNum++;
+                            line = reader.ReadLine();
+                        }
+
+                    }
+                }
+            }
         }
     }
 }
