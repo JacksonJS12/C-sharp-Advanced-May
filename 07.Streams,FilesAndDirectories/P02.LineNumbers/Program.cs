cbd6ccab1ad6e4ea84d3d21daabf27bec70e6e17
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-29 21:50:59 +0300

    Ex02 completed.

---
+++ 07.Streams,FilesAndDirectories/P02.LineNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader("../../../input.txt");
+            using (reader)
+            {
+                var writer = new StreamWriter("ouput.txt");
+                using (writer)
+                {
+                    while (true)
+                    {
+                        int lineNum = 0;
+                        string line = reader.ReadLine();
+                        while (line != null)
+                        {
+                            writer.WriteLine(lineNum + ". " + line);
+                          
+                            lineNum++;
+                            line = reader.ReadLine();
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
