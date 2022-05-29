25ae8b246766a7e6bd4da4b92a2972c192b5ec3e
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-29 20:45:00 +0300

    Commit.

---
+++ 07.Streams,FilesAndDirectories/P01.OddLines/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace P01.OddLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
