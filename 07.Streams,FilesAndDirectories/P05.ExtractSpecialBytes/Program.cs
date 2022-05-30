7b15be0c6e3f08bf976bbcf166d37e670ff62c0e
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-30 22:33:43 +0300

    Commit.

---
+++ 07.Streams,FilesAndDirectories/P05.ExtractSpecialBytes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace P05.ExtractSpecialBytes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const byte secretCode = 243;
+            XorEncrypt(
+                "../../../files/img.jpg",
+                "../../../files/img-encrypted.jpg",
+                secretCode);
+
+            XorEncrypt(
+                "../../../files/img-encrypted.jpg",
+                "../../../files/img-dencrypted.jpg",
+                secretCode);
+            
+        }
+
+        private static void XorEncrypt(string inputFileName, string outPutFileName, byte secretCode)
+        {
+            using (var input = new FileStream(inputFileName, FileMode.Open))
+            {
+                using (var output = new FileStream(outPutFileName, FileMode.Create))
+                {
+                    var buf = new byte[1024];
+                    while (true)
+                    {
+                        var bytesRead = input.Read(buf, 0, buf.Length);
+                        if (bytesRead == 0)
+                            break;
+                        Encrypt(buf, bytesRead, secretCode);
+                        output.Write(buf, 0, bytesRead);
+                    }
+                }
+            }
+        }
+
+        private static void Encrypt(byte[] buf,int read, byte secretCode)
+        {
+            for (int i = 0; i < read; i++)
+            {
+                buf[i] = (byte) (buf[i] ^ secretCode);
+            }
+        }
+    }
+}
