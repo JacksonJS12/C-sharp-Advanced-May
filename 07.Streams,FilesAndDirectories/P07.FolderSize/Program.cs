53f48f03d4467d237f7340981d863403e44807df
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-30 23:22:39 +0300

    Commit.

---
+++ 07.Streams,FilesAndDirectories/P07.FolderSize/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace P07.FolderSize
+{
+    internal class FolderSizeCalculater
+    {
+        static void Main(string[] args)
+        {
+            decimal folderSize = CalcFolderSize("C:/Brother");
+            Console.WriteLine(folderSize );
+            Console.WriteLine("{0:f2} mb", folderSize / (1024 * 1024));
+        }
+
+        static decimal CalcFolderSize(string path)
+        {
+            decimal folderSize = 0;
+            var dirInfo = new DirectoryInfo(path);
+            
+            foreach (var f in dirInfo.EnumerateFiles("*.*", SearchOption.AllDirectories))
+            {
+                folderSize += f.Length;
+            }
+            return folderSize;
+        }
+    }
+}
