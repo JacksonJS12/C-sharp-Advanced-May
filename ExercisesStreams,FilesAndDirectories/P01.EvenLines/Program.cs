4dfa6e3890d50ae2e6a6b7b23109a861398bfd30
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-05-31 23:40:35 +0300

    Commit.

--- ExercisesStreams,FilesAndDirectories/P01.EvenLines/Program.cs
+++ ExercisesStreams,FilesAndDirectories/P01.EvenLines/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
-
-namespace P01.EvenLines
+﻿namespace EvenLines
 {
-    internal class Program
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class EvenLines
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string inputFilePath = @"C:\Users\ricky\source\repos\C-sharp-Advanced-May\ExercisesStreams,FilesAndDirectories";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+        public static string ProcessLines(string inputFilePath)
+        {
+            int counter = 0;
+            StreamReader reader;
+            using (reader = new StreamReader(inputFilePath)) ;
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    line = Replace(line);
+
+                   line = Reverse(line);
+
+                    if (counter % 2 ==0)
+                    {
+
+                    }
+
+                    line = Console.ReadLine();
+                    counter++;
+                }
+            }
+        }
+
+        private static string Reverse(string line)
+        {
+            return string.Join(" ", line.Split().Reverse());
+        }
+
+        private static string Replace(string line)
+        {
+            return line.Replace("-", "@")
+                .Replace(",", "@")
+                .Replace(".", "@")
+                .Replace("!", "@");
         }
     }
 }
