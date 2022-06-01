976af72bb82ec992c3f8d389145580bae9a0bcc0
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-01 15:18:49 +0300

    Commit.

---
+++ ExercisesStreams,FilesAndDirectories/P02.LineNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            int linesCounter = 0;
+            int lethersCounter = 0;
+            int symbolsCounter = 0;
+            foreach (string line in lines)
+            {
+                linesCounter++;
+
+                lethersCounter = line.Count(char.IsLetter);
+
+                symbolsCounter = line.Count(char.IsPunctuation);
+
+                string modifiedLine = $"Line {linesCounter}: {line} ({lethersCounter})({symbolsCounter})";
+            }
+        }
+    }
+}
