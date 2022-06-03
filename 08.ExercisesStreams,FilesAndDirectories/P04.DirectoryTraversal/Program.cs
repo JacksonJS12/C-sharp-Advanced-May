bfa9425e5e1c40aa07c859b9db17b9fab1370312
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-03 22:58:18 +0300

    Commit.

---
+++ 08.ExercisesStreams,FilesAndDirectories/P04.DirectoryTraversal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P04.DirectoryTraversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
