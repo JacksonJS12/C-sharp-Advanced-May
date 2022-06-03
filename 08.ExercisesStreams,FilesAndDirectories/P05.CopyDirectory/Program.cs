4bfaa793e1de28c070c84a06bab48eadff06390f
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-03 22:12:49 +0300

    Exercise 05 completed.

---
+++ 08.ExercisesStreams,FilesAndDirectories/P05.CopyDirectory/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            Directory.CreateDirectory(outputPath);
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (string file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                var copyDestination = Path.Combine(outputPath, fileName);
+                File.Copy(fileName, copyDestination);
+            }
+        }
+    }
+}
