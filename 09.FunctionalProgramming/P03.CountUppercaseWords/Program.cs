8c1d8b40bab7f912e343d8fdc46bf2ee0e08a1dc
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-05 00:17:21 +0300

    Ex 03 completed.

---
+++ 09.FunctionalProgramming/P03.CountUppercaseWords/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Linq;
+
+namespace P03.CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> filter =
+                (string x) => x.Length > 0 && char.IsUpper(x[0]);
+
+            Console.WriteLine(
+                string.Join(
+                   "\r\n",
+                (Console.ReadLine()
+                .Split(" ")
+                .Where(x => filter(x))
+                .ToArray())
+                ));
+        }
+    }
+}
