b0d3d8b1f7eb3151fc28dfc3754269b6f2e83062
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-04 23:19:38 +0300

    Commit.

---
+++ 09.FunctionalProgramming/P02.SumNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Linq;
+
+namespace P02.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parseStringToInt = x => int.Parse(x);
+            Func<int, bool> isEven = x => x % 2 == 0;
+            Func<int, int> indentity = n => n;
+
+            string input = Console.ReadLine();
+            string[] tokens = input.Split(", ");
+            int[] nums = tokens.Select(parseStringToInt).ToArray();
+            int[] evenNums = nums.Where(isEven).ToArray();
+            int[] orderedEvenNums = evenNums.OrderBy(indentity).ToArray();
+
+            Console.WriteLine(string.Join(", ", orderedEvenNums));
+        }
+    }
+}
