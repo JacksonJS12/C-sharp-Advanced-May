229224c6ebb17572f0e3c91057f6ddb47031b275
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-05 00:24:16 +0300

    Ex 04 completed.

---
+++ 09.FunctionalProgramming/P04.AddVAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace P04.AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<decimal, decimal> addVAT = x => x * 1.20m;
+
+            string input = Console.ReadLine();
+            string[] tokens = input.Split(", ");
+            decimal[] nums = tokens.Select(decimal.Parse).ToArray();
+            decimal[] numsWithVAT = nums.Select(addVAT).ToArray();
+
+            Array.ForEach(numsWithVAT, x => Console.WriteLine($"{x:f2}"));
+        }
+    }
+}
