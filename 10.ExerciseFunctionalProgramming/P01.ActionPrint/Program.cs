d45fd85089ea2143af4641c036f3c548bf6bc2fd
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-06 23:05:40 +0300

    Exercise 01 completed.

---
+++ 10.ExerciseFunctionalProgramming/P01.ActionPrint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+
+            Action<string> printNames = name => Console.WriteLine(name);
+            list.ForEach(printNames);
+            
+        }
+    }
+}
