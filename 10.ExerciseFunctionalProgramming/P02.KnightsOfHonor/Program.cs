407a62ecb8abfcb0e6a691d428393bb9b6f7b50b
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-06 23:13:07 +0300

    Exercise 02 completed.

---
+++ 10.ExerciseFunctionalProgramming/P02.KnightsOfHonor/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+
+            Action<string> printNames = names => Console.WriteLine("Sir " + names);
+            list.ForEach(printNames);
+        }
+    }
+}
