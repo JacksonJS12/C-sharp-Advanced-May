5d5c0f25433ce221f284e7bd146132c3a2bfdeb1
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-07 22:09:07 +0300

    Commit.

---
+++ 10.ExerciseFunctionalProgramming/P11.TriFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P11.TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Console.WriteLine(names.First
+                (name => name.Select
+                (symbol => (int)symbol)
+                .Sum() >= n));
+
+
+        }
+    }
+}
