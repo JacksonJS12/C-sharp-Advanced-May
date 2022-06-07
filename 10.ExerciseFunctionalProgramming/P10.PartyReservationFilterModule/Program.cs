1740b29290aace78bb5afdc043349789b43e39a7
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-07 22:03:49 +0300

    Commit.

---
+++ 10.ExerciseFunctionalProgramming/P10.PartyReservationFilterModule/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace P10.PartyReservationFilterModule
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
