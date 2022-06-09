426e4c16b256a52b6ddf6e687026550541d2ff1d
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-09 22:58:26 +0300

    Ex 02 completed.

---
+++ 11.DefiningClasses/P02.CarExtension/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private double fuelQuantity;
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        private double fuelConsumption;
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public void Drive(double distnace)
+        {
+            double consumption = distnace * this.FuelConsumption;
+            if (consumption <= this.fuelQuantity)
+            {
+                this.FuelQuantity -= consumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            string carInfo =
+                $"Make: {this.make}\r\n" +
+                $"Model: {this.Model}\r\n" +
+                $"Year: {this.Year}\r\n" +
+                $"Fuel: {this.FuelQuantity:F2}L";
+            return carInfo;
+        }
+
+    }
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "BMW";
+            car.Model = "X6";
+            car.Year = 2022;
+            car.FuelQuantity = 50;
+            car.FuelConsumption = 0.07;
+            Console.WriteLine(car.WhoAmI());
+            Console.WriteLine();
+
+            car.Drive(700);
+            Console.WriteLine(car.WhoAmI());
+            Console.WriteLine();
+
+            car.Drive(50);
+            Console.WriteLine(car.WhoAmI());
+            Console.WriteLine();
+        }
+    }
+}
