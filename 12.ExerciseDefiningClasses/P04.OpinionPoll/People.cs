dc9de3d9960f4d798a95a29a1112397b2791e061
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-11 00:31:23 +0300

    Ex 04 completed.

---
+++ 12.ExerciseDefiningClasses/P04.OpinionPoll/People.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class People
+    {
+        private List<Person> people = new List<Person>();
+        public List<Person> OrderedList()
+        {
+            List<Person> modifiedListOfPeople = this.people.Where(member => member.Age > 30).ToList();
+            modifiedListOfPeople = modifiedListOfPeople.OrderBy(x => x.Name).ToList();
+            return modifiedListOfPeople;
+        }
+        public void AddMember(Person member)
+        {
+            people.Add(member);
+        }
+    }
+}
---
+++ 12.ExerciseDefiningClasses/P04.OpinionPoll/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age)
+        {
+            this.Age = age;
+            this.Name = "No name";
+        }
+
+    }
+}
