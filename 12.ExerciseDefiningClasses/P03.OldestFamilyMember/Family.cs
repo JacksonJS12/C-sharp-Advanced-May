1e9ce32fe07265d45f2d72047c9b61525473e2a2
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-11 00:06:57 +0300

    Ex 03 completed.

---
+++ 12.ExerciseDefiningClasses/P03.OldestFamilyMember/Family.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    internal class Family
+    {
+        private List<Person> people;
+        public Family()
+        {
+            this.people = new List<Person>();
+        }
+        public void AddMember(Person member)
+        {
+            this.people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            int maxAge = this.people.Max(member => member.Age);
+            return this.people.First(member => member.Age == maxAge);
+        }
+    }
+}
---
+++ 12.ExerciseDefiningClasses/P03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Family family = new Family();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string member = Console.ReadLine();
+                string name = member.Split()[0];
+                int age = int.Parse(member.Split()[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+            Person oldestMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
