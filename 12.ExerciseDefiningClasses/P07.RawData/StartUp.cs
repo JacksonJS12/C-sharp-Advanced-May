b728850f8528972f260d1c0a9a6fce357eddca7b
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-13 23:46:06 +0300

    Commit.

---
+++ 12.ExerciseDefiningClasses/P07.RawData/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            People people = new People();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string member = Console.ReadLine();
+                string name = member.Split()[0];
+                int age = int.Parse(member.Split()[1]);
+
+                Person person = new Person(name, age);
+                people.AddMember(person);
+            }
+            List<Person> modifiedList = people.OrderedList();
+           
+
+            for (int i = 0; i < modifiedList.Count; i++)
+            {
+                Console.WriteLine(modifiedList[i].Name + " - " + modifiedList[i].Age);
+            }
+        }
+    }
+}
