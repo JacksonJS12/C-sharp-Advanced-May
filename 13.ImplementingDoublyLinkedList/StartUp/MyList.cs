685bf7b3bf5955b6f02ed04b5a75a9f17c1adadc
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-18 21:46:30 +0300

    Commit.

--- 13.ImplementingDoublyLinkedList/StartUp/MyList.cs
+++ 13.ImplementingDoublyLinkedList/StartUp/MyList.cs
@@ -22,6 +22,19 @@ namespace StartUp
             this.data = new int[capacity];
         }
         public int Count { get; private set; }
+        public int this[int index]
+        {
+            get
+            {
+                this.ValidateIndex(index);
+                return this.data[index];
+            }
+            set
+            {
+                this.ValidateIndex(index);
+                this.data[index] = value;
+            }
+        }
         public void Add(int number)
         {
             if (this.Count == this.data.Length)
@@ -37,7 +50,7 @@ namespace StartUp
             {
                 return;
             }
-            var message = this.Count == 0 ? "This list is empty" : 
+            var message = this.Count == 0 ? "This list is empty" :
                 $"This list has ${this.Count} elements and it's zero-based and you are trying to access {index} which is not in the list";
             throw new ArgumentException($"Index out of rage. {message}");
         }
@@ -50,22 +63,52 @@ namespace StartUp
             var result = this.data[index];
             for (int i = index + 1; i < this.Count; i++)
             {
-                this.data[i - 1] = this.data[i]; 
+                this.data[i - 1] = this.data[i];
             }
-           
+
             this.Count--;
             return result;
-            
+
         }
-        private void Resize()
+     
+        public void Shift(int index)
         {
-            var newCpacity = this.data.Length * 2;
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.data[i] = this.data[i + 1];
+            }
+        }
+        private int[] ChangeArrayLenght(string operation)
+        {
+            var currentOperation = operation == "*"
+                ? this.data.Length * 2
+                : this.data.Length / 2;
+            var newCpacity = currentOperation;
             var newData = new int[newCpacity];
             for (int i = 0; i < this.data.Length; i++)
             {
                 newData[i] = this.data[i];
             }
-            this.data = newData;
+            return newData;
+        }
+        private void Resize() => this.data = ChangeArrayLenght("*");
+
+        private void Shrink() => this.data = ChangeArrayLenght("/");
+
+        private void ShiftRight(int index)
+        {
+            for (int i = this.Count; i >= index ; i++)
+            {
+                this.data[i + 1] = this.data[i];
+            }
+        }
+        public void Insert (int firstIndex, int secondIndex)
+        {
+            this.ValidateIndex(firstIndex);
+            this.ValidateIndex(secondIndex);
+            var firstValue = this.data[firstIndex];
+            this.data[firstIndex] = this.data[secondIndex];
+            this.data[secondIndex] = firstValue;
         }
     }
 }
--- 13.ImplementingDoublyLinkedList/StartUp/StartUp.cs
+++ 13.ImplementingDoublyLinkedList/StartUp/StartUp.cs
@@ -6,13 +6,26 @@ namespace StartUp
     {
         static void Main(string[] args)
         {
-            var myList = new MyList();
+            var myList = new MyList(3);
             myList.Add(5);
             myList.Add(6);
+            myList.Add(69);
             myList.Add(6);
-            myList.Add(6);
-            myList.Add(6);
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
             Console.WriteLine(myList.Count);
+
+            int removedItem = myList.RemoveAt(2);
+            Console.WriteLine(removedItem);
+            Console.WriteLine(myList.Count);
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
         }
     }
 }
