b04bec9187cb4a9189869a0441133f17a50d4bbe
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-19 16:24:48 +0300

    Commit.

---
+++ 16.ExerciseGenerics/P01.GenericBoxOfString/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class Box<T>
+    {
+        public Box(T element)
+        {
+            this.Element = element;
+        }
+        public T Element { get; private set; }
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this.Element}";
+        }
+    }
+}
---
+++ 16.ExerciseGenerics/P01.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Box
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var input = Console.ReadLine();
+                var box = new Box<string>(input);
+                Console.WriteLine(box.ToString());
+
+            }
+
+        }
+    }
+}
