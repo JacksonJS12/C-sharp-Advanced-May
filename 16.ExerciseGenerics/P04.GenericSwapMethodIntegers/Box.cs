120bf6f8de035e8a2ce6f3dd9532522a9b141ea4
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-19 22:42:41 +0300

    Commit.

---
+++ 16.ExerciseGenerics/P04.GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class Box<T>
+    {
+        public Box(T element)
+        {
+            this.Element = element;
+        }
+        public Box(List<T> elementsList)
+        {
+            this.Elements = elementsList;
+        }
+        public void Swap(List<T> elements, int indexOne, int indexTwo)
+        {
+            T firstElement = elements[indexOne];
+            elements[indexOne] = elements[indexTwo];
+            elements[indexTwo] = firstElement;
+        }
+        public T Element { get; private set; }
+        public List<T> Elements { get; set; }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var element in this.Elements)
+            {
+                sb.Append($"{element.GetType()}: {element}" + Environment.NewLine);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ 16.ExerciseGenerics/P04.GenericSwapMethodIntegers/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+            var list = new List<int>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                list.Add(input);
+
+            }
+            var box = new Box<int>(list);
+            var indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            box.Swap(list, indexes[0], indexes[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
