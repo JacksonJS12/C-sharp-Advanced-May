61e9432463831a573813d5a4a0a83f81667fe760
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-19 16:49:47 +0300

    Commit.

---
+++ 16.ExerciseGenerics/P03.GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+
+            }
+            var box = new Box<string>(list);
+            var indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            box.Swap(list, indexes[0], indexes[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
