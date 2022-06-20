1138f76b7238b48815557188a928601e7839e792
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-20 22:32:40 +0300

    Commit.

---
+++ 16.ExerciseGenerics/P07.Tuple/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P07.Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var personInfo = Console.ReadLine().Split();
+            var fullName = $"{personInfo[0]} {personInfo[1]}";
+            var city = $"{personInfo[2]}";
+
+            var nameAndBeer = Console.ReadLine().Split();
+            var name = nameAndBeer[0];
+            var numberOfLitters = int.Parse(nameAndBeer[1]);
+
+            var numbersInput = Console.ReadLine().Split();
+            var intNum = int.Parse(numbersInput[0]);
+            var doubleNum = double.Parse(numbersInput[1]);
+
+            Tuple<string, string> firstTuple = new Tuple<string, string>(fullName, city);
+            Tuple<string, int> secondTuple = new Tuple<string, int>(name, numberOfLitters);
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(intNum, doubleNum);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ 16.ExerciseGenerics/P07.Tuple/Tuple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P07.Tuple
+{
+    public class Tuple<TFirtst, TSecond>
+    {
+        public Tuple(TFirtst firstElement, TSecond secondElement)
+        {
+            this.FirstElement = firstElement;
+            this.SecondElement = secondElement;
+        }
+        public TFirtst FirstElement { get; private set; }
+        public TSecond SecondElement { get; private set; }
+        public override string ToString()
+        {
+            return $"{this.FirstElement} -> {this.SecondElement}";
+        }
+    }
+}
