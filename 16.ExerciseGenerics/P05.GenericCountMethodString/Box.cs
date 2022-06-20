5bc0d5a96fde9e5b309c348a42620519709491cd
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-20 22:02:34 +0300

    Commit.

---
+++ 16.ExerciseGenerics/P05.GenericCountMethodString/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Box
+{
+    public class Box<T> : IComparable<T> where T : IComparable<T>
+    { 
+        public Box(T element)
+        {
+            this.Element = element;
+        }
+        public Box(List<T> elementsList)
+        {
+            this.Elements = elementsList;
+        }
+        public void Swap(List<T> elements, int indexOne, int indexTwo)
+        {
+            T firstElement = elements[indexOne];
+            elements[indexOne] = elements[indexTwo];
+            elements[indexTwo] = firstElement;
+        }
+        public T Element { get; private set; }
+        public List<T> Elements { get; set; }
+        public int CountOfGreaterElements<T>(List<T> list, T readLine) where T : IComparable =>
+            list.Count(word => word.CompareTo(readLine) > 0);
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var element in this.Elements)
+            {
+                sb.Append($"{element.GetType()}: {element}" + Environment.NewLine);
+            }
+            return sb.ToString();
+        }
+
+        public int CompareTo(T other) => Element.CompareTo(other);
+    }
+}
---
+++ 16.ExerciseGenerics/P05.GenericCountMethodString/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+            //var list = new List<int>();
+
+            //for (int i = 0; i < numberOfLines; i++)
+            //{
+            //    var input = int.Parse(Console.ReadLine());
+            //    list.Add(input);
+
+            //}
+            //var box = new Box<int>(list);
+            //var indexes = Console.ReadLine()
+            //    .Split()
+            //    .Select(int.Parse)
+            //    .ToList();
+
+            //box.Swap(list, indexes[0], indexes[1]);
+            //Console.WriteLine(box);
+
+            var list = new List<string>();
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            var box = new Box<string>(list);
+            var elementToCompare = Console.ReadLine();
+            var count = box.CountOfGreaterElements(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}
