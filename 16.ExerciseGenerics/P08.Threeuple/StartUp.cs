cf0fa506614ff025f623d5e287cc2c2607328d9d
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-20 22:51:31 +0300

    Commit.

---
+++ 16.ExerciseGenerics/P08.Threeuple/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P08.Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var personInfo = Console.ReadLine().Split();
+            var fullName = $"{personInfo[0]} {personInfo[1]}";
+            var address = $"{personInfo[2]}";
+            var city = $"{personInfo[3]}";
+
+            var nameAndBeer = Console.ReadLine().Split();
+            var name = nameAndBeer[0];
+            var numberOfLitters = int.Parse(nameAndBeer[1]);
+            var isDrunken = nameAndBeer[2] == "drunk" == true;
+
+            var nameAndBank = Console.ReadLine().Split();
+            var nameFromBank = nameAndBank[0];
+            var accountBalance = double.Parse(nameAndBank[1]);
+            var bankName = nameAndBank[2];
+
+            Threeuple<string, string, string> firstTuple = new Threeuple<string, string, string>(fullName, address, city);
+            Threeuple<string, int, bool> secondTuple = new Threeuple<string, int, bool>(name, numberOfLitters, isDrunken);
+            Threeuple<string, double, string> thirdTuple = new Threeuple<string, double, string>(nameFromBank, accountBalance, bankName);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ 16.ExerciseGenerics/P08.Threeuple/Threeuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P08.Threeuple
+{
+    public class Threeuple<TFirtst, TSecond, TThirth>
+    {
+        public Threeuple(TFirtst firstElement, TSecond secondElement, TThirth thirthElement)
+        {
+            this.FirstElement = firstElement;
+            this.SecondElement = secondElement;
+            this.ThirthElement = thirthElement;
+        }
+        private TFirtst FirstElement { get; set; }
+        public TSecond SecondElement { get; set; }
+        public TThirth ThirthElement { get; set; }
+        public override string ToString()
+        {
+            return $"{this.FirstElement} -> {this.SecondElement} -> {this.ThirthElement}";
+        }
+    }
+}
