c496d8fa65518b533ce325457e134dd4f723d547
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-20 22:02:56 +0300

    Commit.

--- 16.ExerciseGenerics/P05.GenericCountMethodString/StartUp.cs
+++ 16.ExerciseGenerics/P05.GenericCountMethodString/StartUp.cs
@@ -9,23 +9,7 @@ namespace Box
         static void Main(string[] args)
         {
             var numberOfLines = int.Parse(Console.ReadLine());
-            //var list = new List<int>();
-
-            //for (int i = 0; i < numberOfLines; i++)
-            //{
-            //    var input = int.Parse(Console.ReadLine());
-            //    list.Add(input);
-
-            //}
-            //var box = new Box<int>(list);
-            //var indexes = Console.ReadLine()
-            //    .Split()
-            //    .Select(int.Parse)
-            //    .ToList();
-
-            //box.Swap(list, indexes[0], indexes[1]);
-            //Console.WriteLine(box);
-
+           
             var list = new List<string>();
             for (int i = 0; i < numberOfLines; i++)
             {
