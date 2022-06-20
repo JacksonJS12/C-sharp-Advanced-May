9047b06421674e419eaab6f78c657d7177f2b010
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-20 22:07:29 +0300

    Commit.

---
+++ 16.ExerciseGenerics/P06.GenericCountMethodDoubles/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+           
+            var list = new List<double>();
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+
+            var box = new Box<double>(list);
+            var elementToCompare = double.Parse(Console.ReadLine());
+            var count = box.CountOfGreaterElements(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}
