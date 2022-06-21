e580df1ecb655e1618d880bcc82d80b5bfe0c0f0
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-21 22:02:33 +0300

    Commit.

---
+++ 17.IteratorsAndComparators/P02.LibraryIterator/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("C# for Beginners", 2021, "Peter", "Gosho");
+           
+            Console.WriteLine(book.Title);
+        }
+    }
+}
