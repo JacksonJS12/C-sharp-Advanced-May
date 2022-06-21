03e4f172650c6acebcd21902fa781be15559b430
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-21 22:02:08 +0300

    Commit.

---
+++ 17.IteratorsAndComparators/P01.Library/Library.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library
+    {
+        private Book[] books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books;
+        
+        }
+
+    }
+}
