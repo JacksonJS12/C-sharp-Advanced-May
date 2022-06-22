23febd3631ec00d7891369aebcd22c9e88b4eec9
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-22 23:40:21 +0300

    Commit.

---
+++ 18.ExerciseIteratorsAndComparators/P01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> collection;
+        private int currIndex;
+
+        public ListyIterator(params T[] data)
+        {
+            this.collection = new List<T>(data);
+            this.currIndex = 0;
+        }
+
+        public bool HasNext() => currIndex < collection.Count - 1;
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                currIndex++;
+            }
+            return canMove;
+        }
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation");
+            }
+            else
+            {
+                Console.WriteLine($"{collection[currIndex]}");
+            }
+        }
+    }
+}
---
+++ 18.ExerciseIteratorsAndComparators/P01.ListyIterator/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P01.ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<int> listy = new ListyIterator<int>(1, 2, 3, 4, 5);
+
+            string cmd = string.Empty;
+            
+            while ((cmd = Console.ReadLine()) != "END");
+            {
+
+            }
+        }
+    }
+}
