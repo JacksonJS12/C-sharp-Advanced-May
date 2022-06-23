e669968c8565f417455b08b1b4bb6a9944b15ad6
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-23 21:26:37 +0300

    Commit.

---
+++ 18.ExerciseIteratorsAndComparators/P03.Stack/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03.Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+        public Stack(params T[] data)
+        {
+            this.collection = new List<T>(data);
+        }
+        public void Push(params T[] elements)
+        {
+            foreach (var element in elements)
+            {
+                this.collection.Insert(this.collection.Count, element);
+            }
+        }
+        public T Pop()
+        {
+            if (this.collection.Count == 0)
+            {
+                throw new ArgumentException("No elements");
+            }
+            T element = this.collection[this.collection.Count - 1];
+            this.collection.RemoveAt(this.collection.Count - 1);
+            return element;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.collection.Count - 1; i >= 0; i--)
+            {
+                yield return this.collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ 18.ExerciseIteratorsAndComparators/P03.Stack/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.Stack
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+            while (true)
+            {
+                var tokens = Console.ReadLine().Split();
+                if (tokens[0] == "END")
+                {
+                    break;
+                }
+
+               else if (tokens[0] == "Push")
+                {
+                    stack.Push(tokens
+                        .Skip(1)
+                        .Select
+                        (e => e.Split(",")
+                        .First())
+                        .ToArray());
+                }
+                else if(tokens[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (ArgumentException ae)
+                    {
+                        Console.WriteLine(ae.Message);
+                        throw;
+                    }
+                }
+            }
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
