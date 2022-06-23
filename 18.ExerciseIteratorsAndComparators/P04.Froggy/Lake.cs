b9e85ec5a5e94655d0466f00330bf6100d4ed928
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-23 21:52:53 +0300

    Commit.

---
+++ 18.ExerciseIteratorsAndComparators/P04.Froggy/Lake.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace P04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] lake;
+
+        public Lake(params int[] rocks)
+        {
+            this.lake = rocks;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < lake.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return lake[i];
+                }
+            }
+
+            for (int i = lake.Length - 1; i >= 0; i--)
+            {
+                if(i % 2 != 0)
+                {
+                    yield return lake[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
