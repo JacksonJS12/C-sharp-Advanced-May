22c9f5085bbcdbee785639264e1b2d1a13028bb1
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-24 01:04:14 +0300

    Commit.

---
+++ 18.ExerciseIteratorsAndComparators/P05.ComparingObjects/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace P05.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public Person(string name, int age, string town)
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+        public string Name => name;
+        public int Age => age;
+        public string Town => town;
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+            if (result == 0)
+            {
+                result = this.Town.CompareTo(other.Town);
+            }
+            return result;
+        }
+    }
+}
---
+++ 18.ExerciseIteratorsAndComparators/P05.ComparingObjects/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05.ComparingObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            var toekens = Console.ReadLine().Split();
+            while (toekens[0] != "END")
+            {
+                string personName = toekens[0];
+                int personAge = int.Parse(toekens[1]);
+                string personTown = toekens[2];
+
+                people.Add(new Person(personName, personAge, personTown));
+                toekens = Console.ReadLine().Split();
+            }
+
+            var index = int.Parse(Console.ReadLine()) - 1;
+            var equal = 0;
+            var notEqual = 0;
+
+            foreach (var person in people)
+            {
+                if (people[index].CompareTo(person) == 0)
+                {
+                    equal++;
+                }
+                else
+                {
+                    notEqual++;
+                }
+            }
+            if (equal <= 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equal} {notEqual} {people.Count}");
+            }
+        }
+    }
+}
