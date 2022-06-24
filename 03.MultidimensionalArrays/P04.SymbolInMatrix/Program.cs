c6a474349f76e06428cea9e718214aab38aaa3fe
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-24 16:42:29 +0300

    commit.

--- 03.MultidimensionalArrays/P04.SymbolInMatrix/Program.cs
+++ 03.MultidimensionalArrays/P04.SymbolInMatrix/Program.cs
@@ -6,7 +6,44 @@ namespace P04.SymbolInMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = int.Parse(Console.ReadLine());
+
+            int cols = rows;
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string symbols = Console.ReadLine();
+                for (int col = 0; col < cols; col++)
+                {
+                    char currentSymbol = symbols[col];
+                    matrix[row, col] = currentSymbol;
+                }
+            }
+
+            bool isFound = false;
+
+            char symbol = char.Parse(Console.ReadLine());
+            int symbolInInteger = symbol;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isFound = true;
+                        return;
+                    }
+                }
+            }
+
+            if (isFound == false)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
         }
     }
 }
