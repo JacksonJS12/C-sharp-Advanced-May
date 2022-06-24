12bc8acbc618ffaacd3b43be52e279d087765f11
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-24 16:41:30 +0300

    commit.

---
+++ 03.MultidimensionalArrays/P03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+            long sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                sum += matrix[row, row];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
