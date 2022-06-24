b37eba6b916c174f850216afd6e8262417a43f09
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-24 18:16:13 +0300

    commit.

--- 12.ExerciseDefiningClasses/P07.RawData/StartUp.cs
+++ 12.ExerciseDefiningClasses/P07.RawData/StartUp.cs
@@ -1,31 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Channels;
 
 namespace DefiningClasses
 {
     public class StartUp
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            People people = new People();
+            var cars = CollectCars();
+            PrintCars(cars);
+        }
+
+        private static void PrintCars(List<Car> cars)
+        {
+            string targetCargoType = Console.ReadLine();
+            var filtered = new List<string>();
+            switch (targetCargoType)
+            {
+                case "fragile":
+                    filtered = cars
+                        .Where(car => car.Cargo.Type == "fragile" &&
+                                      car.Tires
+                                          .FirstOrDefault(tire => tire.Pressure < 1) != null)
+                        .Select(car => car.Model)
+                        .ToList();
+                    break;
+                case "flammable":
+                    filtered = cars
+                        .Where(car => car.Cargo.Type == "flammable" &&
+                                      car.Engine.Power > 250)
+                        .Select(car => car.Model)
+                        .ToList();
+                    break;
+            }
+
+            Console.WriteLine(string.Join("\n", filtered));
+
+        }
+
+        private static List<Car> CollectCars()
+        {
+            List<Car> cars = new List<Car>();
             int n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
-                string member = Console.ReadLine();
-                string name = member.Split()[0];
-                int age = int.Parse(member.Split()[1]);
+                string[] carData = Console.ReadLine().Split(' ');
+                string model = carData[0];
+                //"{model} {engineSpeed} {enginePower} {cargoWeight} {cargoType} {tire1Pressure} {tire1Age} {tire2Pressure} {tire2Age} {tire3Pressure} {tire3Age} {tire4Pressure} {tire4Age}"
 
-                Person person = new Person(name, age);
-                people.AddMember(person);
-            }
-            List<Person> modifiedList = people.OrderedList();
-           
+                var engine = new Engine(int.Parse(carData[1]), int.Parse(carData[2]));
+                var cargo = new Cargo(int.Parse(carData[3]), carData[4]);
 
-            for (int i = 0; i < modifiedList.Count; i++)
-            {
-                Console.WriteLine(modifiedList[i].Name + " - " + modifiedList[i].Age);
+                var tires = new Tire[]
+                {
+                    new Tire(double.Parse(carData[5]), int.Parse(carData[6])),
+                    new Tire(double.Parse(carData[7]), int.Parse(carData[8])),
+                    new Tire(double.Parse(carData[9]), int.Parse(carData[10])),
+                    new Tire(double.Parse(carData[11]), int.Parse(carData[12]))
+                };
+
+                Car car = new Car(model, engine, cargo, tires);
+                cars.Add(car);
             }
+
+            return cars;
         }
     }
 }
