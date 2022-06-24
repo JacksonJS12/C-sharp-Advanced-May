c6d5ed8ec42b7539e7b8f7251d42735db95e0f95
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-24 17:16:54 +0300

    commit.

--- 07.Streams,FilesAndDirectories/P04.MergeTextFiles/Program.cs
+++ 07.Streams,FilesAndDirectories/P04.MergeTextFiles/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
-
-namespace P04.MergeTextFiles
+﻿namespace MergeFiles
 {
-    internal class Program
+    using System;
+    using System.IO;
+    public class MergeFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            StreamReader reader1 = new StreamReader(firstInputFilePath);
+            StreamReader reader2 = new StreamReader(secondInputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+
+            using (reader1)
+            {
+                using (reader2)
+                {
+                    using (writer)
+                    {
+                        while (!reader1.EndOfStream || !reader2.EndOfStream)
+                        {
+                            if (reader1 != null)
+                            {
+                                writer.WriteLine(reader1.ReadLine());
+                            }
+                            if (reader2 != null)
+                            {
+                                writer.WriteLine(reader2.ReadLine());
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
