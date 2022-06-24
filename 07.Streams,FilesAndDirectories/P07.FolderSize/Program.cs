86483adfabbaccd5841f13d88d0c5f27ed65bfda
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-24 17:54:39 +0300

    Commit.

--- 07.Streams,FilesAndDirectories/P07.FolderSize/Program.cs
+++ 07.Streams,FilesAndDirectories/P07.FolderSize/Program.cs
@@ -1,39 +1,37 @@
-﻿using System;
-using System.Text;
-using System.IO;
-
-namespace L07_Folder_Size
+﻿namespace FolderSize
 {
-    internal class Program
+    using System;
+    using System.IO;
+    public class FolderSize
     {
         static void Main(string[] args)
         {
-            // We get the directory path of the folder that we want to see the size in bytes.
-            string directoryPath = @"C:\Users\Antoni\Desktop\SoftUni\SoftUni-courses\Advanced Self Preparation\Streams, Files and Directories";
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
 
-            var totalLenght = GetTotalLenghts(directoryPath); // we made a recursive method (method that cals itselc) s owe can not only calculate the files in the main folder, but also its sub-directories. 
-
-            Console.WriteLine(totalLenght);
+            float totalSize = GetFolderSize(folderPath, outputPath);
+            File.WriteAllText(outputPath, $"{totalSize / 1024}");
         }
-        static long GetTotalLenghts(string directoryPath)
+
+        public static float GetFolderSize(string folderPath, string outputFilePath)
         {
-            string[] files = Directory.GetFiles(directoryPath);// We make an array that contains each file name
-            long totalLenght = 0;
+            float folderSize = 0;
 
-            foreach (string file in files)
+            foreach (var file in Directory.GetFiles(folderPath))
             {
-                totalLenght += new FileInfo(file).Length; //we itterate trhough each file in the array and add it to totalLenght to calc all files size and add it to the variable (and the problem is solved to here)
-
+                FileInfo fileInfo = new FileInfo(file);
+                folderSize += fileInfo.Length;
             }
 
-            // However if we want to include the sub directories we have to call the method (recursive) in itself 
-            string[] subDirectories = Directory.GetDirectories(directoryPath);// we get each of the sub direactories in an array and we itterate them and add them to the totalLenght with the method itself. 
-            foreach (var directory in subDirectories)
+            var subDirectories = Directory.GetDirectories(folderPath);
+
+            foreach (var subDirectory in subDirectories)
             {
-                totalLenght += GetTotalLenghts(directory);
+                folderSize += GetFolderSize(subDirectory, outputFilePath);
             }
 
-            return totalLenght;
+
+            return folderSize;
         }
     }
 }
