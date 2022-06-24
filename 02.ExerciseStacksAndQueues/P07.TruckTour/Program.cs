302fedff1a9e1d5c4d70613bb6c678f3ff3af4cd
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-24 16:35:55 +0300

    Commit.

--- 02.ExerciseStacksAndQueues/P07.TruckTour/Program.cs
+++ 02.ExerciseStacksAndQueues/P07.TruckTour/Program.cs
@@ -8,52 +8,60 @@ namespace P07.TruckTour
     {
         class PetrolPump
         {
-            public PetrolPump(int number, int amountOfPetrolForPump, int dictanceBetweenFromPetrolPumpToAnother)
+            static void Main(string[] args)
             {
-                this.Number = number;
-                this.AmountOfPetrolForPump = amountOfPetrolForPump;
-                this.DictanceBetweenFromPetrolPumpToNext = dictanceBetweenFromPetrolPumpToAnother;
-            }
-            public int Number { get; set; }
-            public int AmountOfPetrolForPump { get; set; }
-            public int DictanceBetweenFromPetrolPumpToNext { get; set; }
-        }
-        static void Main(string[] args)
-        {
-            int n = int.Parse(Console.ReadLine());
-            Queue<PetrolPump> petrolPumpsQueue = new Queue<PetrolPump>();
-
-            for (int i = 0; i <= n-1; i++)
-            {
-                int[] petrolPumpsInfo = Console.ReadLine()
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse)
-                    .ToArray();
-
-                int amountOfPetrolForPump = petrolPumpsInfo[0];
-                int dictanceBetweenFromPetrolPumpToAnother = petrolPumpsInfo[1];
-
-                PetrolPump petrolPump = new PetrolPump(i, amountOfPetrolForPump, dictanceBetweenFromPetrolPumpToAnother);
-                petrolPumpsQueue.Enqueue(petrolPump);
-            }
-            int totalDistance = petrolPumpsQueue.Sum(petrolPump => petrolPump.DictanceBetweenFromPetrolPumpToNext);
-            int truckDistance = 0;
-
-            while (true)
-            {
-            int truckFuel = 0;
-                foreach (var petrolPump in petrolPumpsQueue)
+                int numbersOfPumps = int.Parse(Console.ReadLine());
+                var queueForPetrol = new Queue<int>();
+                var queueForDistance = new Queue<int>();
+                var queueForIndex = new Queue<int>();
+                for (int i = 0; i < numbersOfPumps; i++)
                 {
-                    truckFuel += petrolPump.AmountOfPetrolForPump;
-                    if (truckFuel - petrolPump.DictanceBetweenFromPetrolPumpToNext > 0)
-                    {
-
-                    }
-                    truckDistance += petrolPump.DictanceBetweenFromPetrolPumpToNext;
+                    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                    int quantityPetrolInThePump = input[0];
+                    queueForPetrol.Enqueue(quantityPetrolInThePump);
+                    int distanceBetwenTheNext = input[1];
+                    queueForDistance.Enqueue(distanceBetwenTheNext);
+                    queueForIndex.Enqueue(i);
                 }
-               
+
+                int amountPetrolInTheTruck = 0;
+                int countPassedPump = 0;
+                int index = 0;
+                while (countPassedPump != numbersOfPumps + 1) // condition to complete the circle
+                {
+                    int petrol = queueForPetrol.Peek();
+                    int distance = queueForDistance.Peek();
+                    index = queueForIndex.Peek();
+                    amountPetrolInTheTruck += petrol;
+                    if (amountPetrolInTheTruck >= distance)
+                    {
+                        countPassedPump++;
+                        MoveFirstElementsToTheLastPositions(queueForPetrol, queueForDistance, queueForIndex, index, petrol, distance);
+                        amountPetrolInTheTruck -= distance;
+                    }
+
+                    else if (amountPetrolInTheTruck < distance)
+                    {
+                        countPassedPump = 0;
+                        amountPetrolInTheTruck = 0;
+                        MoveFirstElementsToTheLastPositions(queueForPetrol, queueForDistance, queueForIndex, index, petrol, distance);
+                    }
+                }
+
+                Console.WriteLine(index);
             }
-            Console.WriteLine();
+
+
+            private static void MoveFirstElementsToTheLastPositions(Queue<int> queueForPetrol, Queue<int> queueForDistance, Queue<int> queueForIndex, int index, int petrol, int distance)
+            {
+                queueForDistance.Dequeue();
+                queueForPetrol.Dequeue();
+                queueForIndex.Dequeue();
+                queueForDistance.Enqueue(distance);
+                queueForPetrol.Enqueue(petrol);
+                queueForIndex.Enqueue(index);
+            }
         }
     }
+
 }
