b8dab4bc165a5172e0b7505c17a181adad9359ba
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-24 21:28:10 +0300

    Commit.

---
+++ 19.ExamPreparation/P01.MealPlan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.MealPlan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
