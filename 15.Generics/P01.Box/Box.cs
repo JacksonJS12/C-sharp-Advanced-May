34c623dfd4b580f05cb2e7f9a4dcaa05e36bfe03
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-24 18:10:34 +0300

    commit.

--- 15.Generics/P01.Box/Box.cs
+++ 15.Generics/P01.Box/Box.cs
@@ -1,40 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BoxOfT
 {
     public class Box<T>
     {
-        private readonly List<T> data;
+        private List<T> internalList = new List<T>();
 
-        public Box()
+        public int Count => internalList.Count;
+
+        public void Add(T element)
         {
-            this.data = new List<T>();
-        }
-        public void Add(T item)
-        {
-            this.data.Add(item);
+            this.internalList.Add(element);
         }
 
         public T Remove()
         {
-            if (this.data.Count == 0)
-            {
-                throw new InvalidOperationException("The box is empty.");
-            }
-            var lastElement = this.data[this.data.Count - 1];
-            this.data.RemoveAt(this.data.Count - 1);
-            return lastElement;
-        }
-        public  int Count
-        {
-            get
-            {
-                return this.data.Count;
-            }
+            var element = internalList[internalList.Count - 1];
+            internalList.RemoveAt(internalList.Count - 1);
+            return element;
         }
     }
 }
