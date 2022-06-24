4d92e3f1054157dd1aaea3b52f1384fe24f670a1
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-24 17:59:09 +0300

    commit.

---
+++ 10.ExerciseFunctionalProgramming/P07.PredicateForNames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetLength = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(' ').ToList();
+
+            Func<string, bool> function = name => name.Length <= targetLength;
+
+            names = names.Where(function).ToList();
+
+            Action<List<string>> print = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            print(names);
+        }
+    }
+}
