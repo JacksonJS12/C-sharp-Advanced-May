ed969e6e097d93de491de998e6ef709087d0a8b9
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-25 08:54:15 +0300

    Commit.

---
+++ 21.RegularExam/P01.Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
