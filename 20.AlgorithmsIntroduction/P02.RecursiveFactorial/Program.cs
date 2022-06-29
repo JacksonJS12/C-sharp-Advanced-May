936e0483d6afcc20893d08d9a4c850826c49cc4f
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-06-29 23:57:38 +0300

    Commit.

---
+++ 20.AlgorithmsIntroduction/P02.RecursiveFactorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P02.RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
