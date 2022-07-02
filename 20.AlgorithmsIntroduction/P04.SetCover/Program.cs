a44ae8cca742498c9d5e48bf4382c2e78c06bc23
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-02 23:21:48 +0300

    Commit.

---
+++ 20.AlgorithmsIntroduction/P04.SetCover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P04.SetCover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
