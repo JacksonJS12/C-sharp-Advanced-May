0e678fb1bcec6716c22b5ad712de3c1bb2340bb0
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-07-06 23:45:29 +0300

    Commit.

---
+++ 20.AlgorithmsIntroduction/P06.Quicksort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P06.Quicksort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
