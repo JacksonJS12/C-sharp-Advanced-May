9437526ef64219a90b1316e05ea8e456db6b5424
Deniz Ivanov <d.ivan0v6100@gmail.com>
2022-08-17 14:12:46 +0300

    init

---
+++ RetakeExamPreparation23October2021/P01.FoodFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.FoodFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
