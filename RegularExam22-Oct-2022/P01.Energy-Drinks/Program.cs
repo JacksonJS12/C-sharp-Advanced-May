381ac446e5906b7dbb08126d32c8b55c40cd14ca
Deniz <d.ivan0v.6100@gmail.com>
2022-10-22 19:45:03 +0300

    fix

---
+++ RegularExam22-Oct-2022/P01.Energy-Drinks/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Energy_Drinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MaxCaffeinе = 300;
+            int totalCaffeinе = 0;
+
+            var milligramsOfCaffeinе = new Stack<int>();
+            var energyDrinks = new Queue<int>();
+
+            var milligramsOfCaffeinеInput = Console.ReadLine()
+                 .Split(", ")
+                 .Select(int.Parse)
+                 .ToArray();
+
+            foreach (var item in milligramsOfCaffeinеInput)
+            {
+                milligramsOfCaffeinе.Push(item);
+            }
+
+            var energyDrinksInput = Console.ReadLine()
+                 .Split(", ")
+                 .Select(int.Parse)
+                 .ToArray();
+
+            foreach (var item in energyDrinksInput)
+            {
+                energyDrinks.Enqueue(item);
+            }
+
+            while (true)
+            {
+                if (milligramsOfCaffeinе.Count == 0 ||
+                    energyDrinks.Count == 0)
+                {
+                    break;
+                }
+                int currMilligramsOfCaffeinе = milligramsOfCaffeinе.Pop();
+                int currenergyDrink = energyDrinks.Dequeue();
+
+                int caffeinеSum = currMilligramsOfCaffeinе * currenergyDrink;
+                if (caffeinеSum >= MaxCaffeinе - totalCaffeinе)
+                {
+                    totalCaffeinе -= 30;
+                    energyDrinks.Enqueue(currenergyDrink);
+                    continue;
+                }
+
+                totalCaffeinе += caffeinеSum;
+            }
+
+            if (energyDrinks.Count > 0)
+            {
+                Console.WriteLine($"Drinks left: {String.Join(", ", energyDrinks)}");
+            }
+            else
+            {
+                Console.WriteLine("At least Stamat wasn't exceeding the maximum caffeine.");
+            }
+
+            Console.WriteLine($"Stamat is going to sleep with {totalCaffeinе} mg caffeine.");
+        }
+    }
+}
---
+++ RegularExam22-Oct-2022/P03.Computer-Architecture/CPU.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class CPU
+    {
+        private string brand;
+        private int cores;
+        private double frequency;
+        public CPU(string brand, int cores, double frequency)
+        {
+            this.Brand = brand;
+            this.Cores = cores;
+            this.Frequency = frequency;
+        }
+
+        public string Brand 
+        {
+            get
+            {
+                return this.brand;
+            }
+            set
+            {
+                this.brand = value;
+            }
+        }
+        public int Cores 
+        {
+            get
+            {
+                return this.cores;
+            }
+            set
+            {
+                this.cores = value;
+            }
+        }
+        public double Frequency  
+        {
+            get
+            {
+                return this.frequency;
+            }
+            set
+            {
+                this.frequency = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb
+                .AppendLine($"{this.Brand} CPU:")
+                .AppendLine($"Cores: {this.Cores}")
+                .AppendLine($"Frequency: {this.Frequency:f1} GHz");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
