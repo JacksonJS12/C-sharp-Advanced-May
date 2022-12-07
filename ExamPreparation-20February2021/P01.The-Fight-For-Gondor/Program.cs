883e251ea0435e2298c0895c79cb72e62d4c6fff
Deniz <d.ivan0v.6100@gmail.com>
2022-12-07 23:46:04 +0200

    init

---
+++ ExamPreparation-20February2021/P01.The-Fight-For-Gondor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.The_Fight_For_Gondor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ExamPreparation-20February2021/P03.The-Race/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P03.The_Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
