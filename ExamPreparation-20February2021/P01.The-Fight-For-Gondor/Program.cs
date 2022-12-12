1d6c53f55df945ff95df8ad4bceaa91dd529559c
Deniz <d.ivan0v.6100@gmail.com>
2022-12-12 22:30:52 +0200

    Commit

--- ExamPreparation-20February2021/P01.The-Fight-For-Gondor/Program.cs
+++ ExamPreparation-20February2021/P01.The-Fight-For-Gondor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P01.The_Fight_for_Gondor
 {
@@ -6,7 +8,66 @@ namespace P01.The_Fight_for_Gondor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfWaves = int.Parse(Console.ReadLine());
+
+            Stack<int> plates = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+
+            Stack<int> orcs = new Stack<int>();
+            for (int i = 1; i <= numberOfWaves; i++)
+            {
+                var currWave = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse);
+
+                foreach (var orc in currWave)
+                {
+                    orcs.Push(orc);
+                }
+
+                if (i % 3 == 0)
+                {
+                    int plate = int.Parse(Console.ReadLine());
+                    var platesAsList = plates.ToList();
+                    platesAsList.Add(plate);
+                    platesAsList.Reverse();
+                    plates = new Stack<int>(platesAsList);
+                }
+
+                while (plates.Count > 0 && orcs.Count > 0)
+                {
+                    int currPlate = plates.Pop();
+                    int currOrc = orcs.Pop();
+
+                    if (currPlate > currOrc)
+                    {
+                        currPlate -= currOrc;
+                        plates.Push(currPlate);
+                    }
+                    else if (currPlate < currOrc)
+                    {
+                        currOrc -= currPlate;
+                        orcs.Push(currOrc);
+                    }
+                }
+
+                if (plates.Count <= 0)
+                {
+                    break;
+                }
+            }
+
+            if (plates.Count > 0)
+            {
+                Console.WriteLine("The people successfully repulsed the orc's attack.");
+                Console.WriteLine($"Plates left: {string.Join(", ", plates)}");
+            }
+            else
+            {
+                Console.WriteLine("The orcs successfully destroyed the Gondor's defense.");
+                Console.WriteLine($"Orcs left: {string.Join(", ", orcs)}");
+            }
         }
     }
 }
