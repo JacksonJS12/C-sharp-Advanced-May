8e5fef918f51da1007a2d67b9e7a70c8ecab213e
Deniz <d.ivan0v.6100@gmail.com>
2022-12-13 21:46:25 +0200

    90/100

--- ExamPreparation25June2022/P01.Tiles-Master/Program.cs
+++ ExamPreparation25June2022/P01.Tiles-Master/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace P01.Tiles_Master
 {
@@ -6,7 +10,88 @@ namespace P01.Tiles_Master
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tilesForTheLocations = new Dictionary<string, int>() {
+                {"Countertop", 60},
+                {"Oven", 50},
+                {"Sink", 40},
+                {"Wall", 70}
+            };
+
+            var tilesForTheLocationsCounter = new Dictionary<string, int>() {
+                {"Floor", 0},
+                {"Countertop", 0},
+                {"Oven", 0},
+                {"Sink", 0},
+                {"Wall", 0}
+            };
+
+            var whiteTiles = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+
+            var greyTiles = new Queue<int>(
+                Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+
+            while (true)
+            {
+                int currWhiteTile = whiteTiles.Pop();
+                int currGreyTile = greyTiles.Dequeue();
+
+                if (currWhiteTile == currGreyTile)
+                {
+                    int tilesSum = currGreyTile + currWhiteTile;
+                    bool flag = false;
+                    foreach (var location in tilesForTheLocations)
+                    {
+                        if (location.Value == tilesSum)
+                        {
+                            foreach (var locationCounter in tilesForTheLocationsCounter)
+                            {
+                                if (locationCounter.Key == location.Key)
+                                {
+                                    tilesForTheLocationsCounter[locationCounter.Key] += 1;
+                                    flag = true;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    if (!flag)
+                    {
+                        tilesForTheLocationsCounter["Floor"] += 1;
+                    }
+                    
+                }
+                else
+                {
+                    currWhiteTile /= 2;
+                    whiteTiles.Push(currWhiteTile);
+
+                    greyTiles.Enqueue(currGreyTile);
+                }
+
+                if (whiteTiles.Count <= 0 || greyTiles.Count <= 0)
+                {
+                    break;
+                }
+            }
+
+            string whiteTilesRes = whiteTiles.Count > 0 ? $"White tiles left: {string.Join(", ", whiteTiles)}" : "White tiles left: none";
+            Console.WriteLine(whiteTilesRes);
+
+            string greyTilesRes = greyTiles.Count > 0 ? $"Grey tiles left: {string.Join(", ", greyTiles)}" : "Grey tiles left: none";
+            Console.WriteLine(greyTilesRes);
+
+            foreach (var location in tilesForTheLocationsCounter)
+            {
+                if (location.Value != 0)
+                {
+                    Console.WriteLine($"{location.Key}: {location.Value}");
+                }
+            }
         }
     }
 }
