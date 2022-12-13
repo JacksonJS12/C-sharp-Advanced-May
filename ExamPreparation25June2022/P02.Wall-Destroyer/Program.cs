f2766f059a17598ef76a680ef2b2b0f4afae63d2
Deniz <d.ivan0v.6100@gmail.com>
2022-12-13 22:51:47 +0200

    90/100 ;/

--- ExamPreparation25June2022/P02.Wall-Destroyer/Program.cs
+++ ExamPreparation25June2022/P02.Wall-Destroyer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P02.Wall_Destroyer
 {
@@ -6,7 +7,140 @@ namespace P02.Wall_Destroyer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rows = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[rows][];
+
+            #region Fill The Matrix
+            for (int r = 0; r < rows; r++)
+            {
+                char[] line = Console.ReadLine()
+                    .ToCharArray();
+
+                matrix[r] = line;
+            }
+            #endregion
+
+            bool flag = false;
+            int rotCounter = 0; 
+            int holesCounter = 0;
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                
+                if (cmd == "End")
+                {
+                    break;
+                }
+
+                //Vanko position
+                int vankoCurrRow = 0;
+                int vankoCurrCol = 0;
+
+                #region Read Vanko's position
+                for (int r = 0; r < rows; r++)
+                {
+                    for (int c = 0; c < rows; c++)
+                    {
+                        if (matrix[r][c] == 'V')
+                        {
+                            vankoCurrRow = r;
+                            vankoCurrCol = c;
+                        }
+                        else if (matrix[r][c] == 'E')
+                        {
+                            flag = true;
+                        }
+                    }
+                }
+                //if he gets electrocuted
+                if (flag)
+                {
+                    break;
+                }
+                #endregion
+
+                #region Move
+                if (cmd == "up" && vankoCurrRow - 1 >= 0)
+                {
+                    if (matrix[vankoCurrRow - 1][vankoCurrCol] == 'R')
+                    {
+                        rotCounter++;
+                    }
+                    Move(vankoCurrRow, vankoCurrCol, vankoCurrRow - 1, vankoCurrCol, matrix);
+                }
+                else if (cmd == "down" && vankoCurrRow + 1 <= rows)
+                {
+                    if (matrix[vankoCurrRow + 1][vankoCurrCol] == 'R')
+                    {
+                        rotCounter++;
+                    }
+                    Move(vankoCurrRow, vankoCurrCol, vankoCurrRow + 1, vankoCurrCol, matrix);
+                }
+                else if (cmd == "left" && vankoCurrCol - 1 >= 0)
+                {
+                    if (matrix[vankoCurrRow][vankoCurrCol - 1] == 'R')
+                    {
+                        rotCounter++;
+                    }
+                    Move(vankoCurrRow, vankoCurrCol, vankoCurrRow, vankoCurrCol - 1, matrix);
+                }
+                else if (cmd == "right" && vankoCurrCol + 1 <= rows)
+                {
+                    if (matrix[vankoCurrRow][vankoCurrCol + 1] == 'R')
+                    {
+                        rotCounter++;
+                    }
+                    Move(vankoCurrRow, vankoCurrCol, vankoCurrRow, vankoCurrCol + 1, matrix);
+                }
+                #endregion
+            }
+
+            
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < rows; c++)
+                {
+                    if (matrix[r][c] == '*' || matrix[r][c] == 'E' || matrix[r][c] == 'V')
+                    {
+                        holesCounter++;
+                    }
+                }
+            }
+
+            if (flag)
+            {
+                Console.WriteLine($"Vanko got electrocuted, but he managed to make {holesCounter} hole(s).");
+            }
+            else
+            {
+                Console.WriteLine($"Vanko managed to make {holesCounter} hole(s) and he hit only {rotCounter} rod(s).");
+            }
+
+            foreach (var line in matrix)
+            {
+                Console.WriteLine(string.Join("", line));
+            }
+        }
+        public static void Move(int vankoCurrRow, int vankoCurrCol, int vankoNewRow, int vankoNewCol, char[][] matrix)
+        {
+            if (matrix[vankoNewRow][vankoNewCol] == 'R')
+            {
+                Console.WriteLine("Vanko hit a rod!");
+                return;
+            }
+            else if (matrix[vankoNewRow][vankoNewCol] == 'C')
+            {
+                matrix[vankoNewRow][vankoNewCol] = 'E';
+                matrix[vankoCurrRow][vankoCurrCol] = '*';
+                return;
+            }
+            else if (matrix[vankoNewRow][vankoNewCol] == '*')
+            {
+                Console.WriteLine($"The wall is already destroyed at position [{vankoNewRow}, {vankoNewCol}]!");
+            }
+    
+            matrix[vankoCurrRow][vankoCurrCol] = '*';
+            matrix[vankoNewRow][vankoNewCol] = 'V';
         }
     }
 }
