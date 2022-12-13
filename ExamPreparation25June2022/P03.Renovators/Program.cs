ed233f1698936a30d81c5efc6fabb2227b9d2802
Deniz <d.ivan0v.6100@gmail.com>
2022-12-13 20:50:34 +0200

    init

---
+++ ExamPreparation25June2022/P03.Renovators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Renovators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
